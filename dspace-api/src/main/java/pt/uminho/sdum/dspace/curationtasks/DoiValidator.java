682fb31867d951a7236fc90616e73539c93869fd
Tiago Guimarães <tmmguimaraes@sdum.uminho.pt>
2016-07-08 11:44:00 +0100

    fixed doi error because of string index

--- dspace-api/src/main/java/pt/uminho/sdum/dspace/curationtasks/DoiValidator.java
+++ dspace-api/src/main/java/pt/uminho/sdum/dspace/curationtasks/DoiValidator.java
@@ -30,14 +30,20 @@ public class DoiValidator extends Distributor {
                 sb.append("</a> não tem o prefixo do DOI (http://dx.doi.org/)  <br>");
                 //got regex from http://blog.crossref.org/2015/08/doi-regular-expressions.html
             } else { */
-                String sub = doi.substring(18);
-                if (!sub.matches("^10.\\d{4,9}/[-._;()/:a-zA-Z0-9]+$")) {
-                    sb.append("<a href=\"http://hdl.handle.net/");
-                    sb.append(handle);
-                    sb.append("\" target=\"_blank\">");
-                    sb.append(handle);
-                    sb.append("</a> pode ter um DOI inválido <br>");
-                }
+            String sub;
+            if (doi.startsWith("http://dx.doi.org/"))
+                sub = doi.substring(18);
+            else if(doi.startsWith("http://doi.org/"))
+                sub = doi.substring(15);
+            else
+                sub = doi;
+            if (!sub.matches("^10.\\d{4,9}/[-._;()/:a-zA-Z0-9]+$")) {
+                sb.append("<a href=\"http://hdl.handle.net/");
+                sb.append(handle);
+                sb.append("\" target=\"_blank\">");
+                sb.append(handle);
+                sb.append("</a> pode ter um DOI inválido <br>");
+            }
             //}
         }
 
