df5b942136f4d5095d32b857244ae97c2e8ed56e
Tiago Guimarães <tmmguimaraes@sdum.uminho.pt>
2016-07-25 15:14:53 +0100

    Only Restricted after workflow tasks

---
+++ dspace-api/src/main/java/pt/uminho/sdum/submit/RestrictAccess.java
@@ -0,0 +1,66 @@
+package pt.uminho.sdum.submit;
+
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.authorize.AuthorizeManager;
+import org.dspace.authorize.ResourcePolicy;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Bundle;
+import org.dspace.content.Item;
+import org.dspace.core.Context;
+import org.dspace.eperson.Group;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by tmmguimaraes on 25/07/2016.
+ */
+public class RestrictAccess {
+
+    private static final Date date = policyDate();
+
+    private static Date policyDate(){
+        Date d = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+            d = sdf.parse("9999");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return d;
+    }
+
+
+
+    public static void restrictItemAccess(Context context,Item item){
+
+        if(item.getMetadata("dc.rights").equals("restrictedAccess")
+                || item.getMetadata("dc.rights").equals("closedAccess")) {
+
+            Group anonGroup  = null;
+            try {
+                anonGroup = Group.find(context, Group.ANONYMOUS_ID);
+
+            for (Bundle b : item.getBundles()) {
+                if(b.getName().startsWith("ORIG")) {
+                    AuthorizeManager.removeGroupPolicies(context, b, anonGroup);
+                    for (Bitstream bit : b.getBitstreams()) {
+                        for(ResourcePolicy rp: AuthorizeManager.getPolicies(context,bit)) {
+                            rp.setStartDate(date);
+                            rp.update();
+                        }
+                    }
+                }
+            }
+
+            context.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (AuthorizeException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
